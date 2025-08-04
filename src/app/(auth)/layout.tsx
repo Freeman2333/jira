673597e4bff89a850d54642400549e7fe8244403
Serhii Popov <serhii.popov@codeit.pro>
2025-08-04 11:08:31 +0300

    app started & auth added

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React from "react";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+function AuthLayout({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
+  return (
+    <main className="bg-neutral-100 min-h-screen">
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image priority src="/logo.svg" alt="logo" width={152} height={56} />
+          <Button asChild variant="secondary">
+            <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+              {isSignIn ? "Sign Up" : "Login"}
+            </Link>
+          </Button>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default AuthLayout;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import { SignUpCard } from "../components/sign-up-card";
+
+const SignUpPage = async () => {
+  return <SignUpCard />;
+};
+
+export default SignUpPage;
