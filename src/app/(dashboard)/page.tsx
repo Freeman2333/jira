5149143b348cfdebbaa903ddbe0e6bce7b2a8a89
Serhii Popov <serhii.popov@codeit.pro>
2025-08-06 12:16:32 +0300

    workspace form built

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,16 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
+import CreateWorkspaceForm from "@/features/dashboard/components/create-workspace-form";
 
 export default async function Home() {
   const user = await getCurrent();
 
   if (!user) redirect("/sign-in");
 
-  return <div>home dashboard</div>;
+  return (
+    <div>
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
