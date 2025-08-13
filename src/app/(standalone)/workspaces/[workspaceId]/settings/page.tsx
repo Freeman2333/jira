d11cc5aae463168f6af5c528c13bf99df7f2776b
Serhii Popov <serhii.popov@codeit.pro>
2025-08-13 10:33:21 +0300

    feat: implement workspace settings page with update functionality and validation

---
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -0,0 +1,29 @@
+import { getWorkspace } from "@/app/(dashboard)/queries";
+import { getCurrent } from "@/features/auth/actions";
+import EditWorkspaceForm from "@/features/workspaces/components/edit-workspace-form";
+import { redirect } from "next/navigation";
+
+interface WorkspaceIdSettingsPageProps {
+  params: { workspaceId: string };
+}
+
+const WorkspaceIdSettingPage = async ({
+  params,
+}: WorkspaceIdSettingsPageProps) => {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+  console.log("initialValues", initialValues);
+
+  if (!initialValues) redirect("/workspaces");
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <EditWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
+};
+
+export default WorkspaceIdSettingPage;
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { ID, Query } from "node-appwrite";
 
-import { createWorkspaceSchema } from "../schemas";
+import { createWorkspaceSchema, updateWorkspaceSchema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import {
   DATABASE_ID,
@@ -12,6 +12,7 @@ import {
 } from "@/config";
 import { MemberRole } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
+import { getMember } from "@/features/members/utils";
 
 const app = new Hono()
   .get("/", sessionMiddleware, async (c) => {
@@ -97,6 +98,78 @@ const app = new Hono()
         workspaceId: workspace.$id,
         role: MemberRole.ADMIN,
       });
+
+      return c.json({
+        data: workspace,
+      });
+    }
+  )
+  .patch(
+    "/:workspaceId",
+    zValidator("form", updateWorkspaceSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { workspaceId } = c.req.param();
+      const { name, image } = c.req.valid("form");
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member || member.role !== MemberRole.ADMIN) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      const getExtensionFromType = (type: string) => {
+        const match = type.match(/\/([a-zA-Z0-9]+)/);
+        return match ? `.${match[1]}` : "";
+      };
+
+      const fileImage =
+        image instanceof Blob && !(image instanceof File)
+          ? new File(
+              [image],
+              `${name}${getExtensionFromType(image.type || "image/png")}`,
+              { type: image.type || "image/png" }
+            )
+          : image;
+
+      if (fileImage instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          fileImage
+        );
+
+        const arrayBuffer = await storage.getFileView(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      } else {
+        uploadedImageUrl = image;
+      }
+
+      const workspace = await databases.updateDocument(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        workspaceId,
+        {
+          name,
+          imageUrl: uploadedImageUrl,
+        }
+      );
 
       return c.json({
         data: workspace,
