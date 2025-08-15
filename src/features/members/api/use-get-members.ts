6a83c702a51037989e70c52f7a015adb22e66c33
Serhii Popov <serhii.popov@codeit.pro>
2025-08-15 09:31:51 +0300

    feat: implement members management functionality with CRUD operations and UI components

---
+++ src/features/members/api/use-get-members.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { client } from "@/lib/rpc";
+
+interface useGetMembersProps {
+  workspaceId: string;
+}
+
+export const useGetMembers = ({ workspaceId }: useGetMembersProps) => {
+  const query = useQuery({
+    queryKey: ["members"],
+    queryFn: async () => {
+      const res = await client.api.members["$get"]({
+        query: { workspaceId },
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to fetch members.");
+      }
+
+      const { data } = await res.json();
+      return data;
+    },
+  });
+  return query;
+};
