0b78a5da861c63d42d821f6813d331b85850cb26
Serhii Popov <serhii.popov@codeit.pro>
2025-08-22 10:40:46 +0300

    feat: implement task management features with data table, task actions, and date display

--- src/features/tasks/server/route.ts
+++ src/features/tasks/server/route.ts
@@ -10,7 +10,7 @@ import { createAdminClient } from "@/lib/appwrite";
 import { getMember } from "@/features/members/utils";
 
 import { createTaskSchema } from "../schemas";
-import { TaskStatus } from "../types";
+import { Task, TaskStatus } from "../types";
 import { Project } from "@/features/projects/type";
 
 const app = new Hono()
@@ -76,7 +76,11 @@ const app = new Hono()
         query.push(Query.search("name", search));
       }
 
-      const tasks = await databases.listDocuments(DATABASE_ID, TASKS_ID, query);
+      const tasks = await databases.listDocuments<Task>(
+        DATABASE_ID,
+        TASKS_ID,
+        query
+      );
 
       const projectIds = tasks.documents.map((task) => task.projectId);
       const assigneeIds = tasks.documents.map((task) => task.assigneeId);
