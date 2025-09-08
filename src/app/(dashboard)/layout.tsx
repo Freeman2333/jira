b7c7016be316a294ff2bb7431869c8348bbd2985
Serhii Popov <serhii.popov@codeit.pro>
2025-09-08 10:59:56 +0300

    task page added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,7 +18,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full">
           <Sidebar />
         </div>
-        <div className="lg:pl-[264px] w-full">
+        <div className="lg:pl-[264px] w-full h-full">
           <div className="mx-auto max-w-screen-2xl h-full">
             <Navbar />
             <main className="h-full py-8 px-6 flex flex-col">{children}</main>
